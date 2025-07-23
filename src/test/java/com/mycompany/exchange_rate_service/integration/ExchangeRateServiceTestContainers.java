d6d449f93bdf73d715624806863d82b30f18ee14
dany.cenas <dany.cenas@bbva.com>
2025-07-22 20:54:38 -0500

    .

--- src/test/java/com/mycompany/exchange_rate_service/integration/ExchangeRateServiceTestContainers.java
+++ src/test/java/com/mycompany/exchange_rate_service/integration/ExchangeRateServiceTestContainers.java
@@ -45,6 +45,7 @@ public class ExchangeRateServiceTestContainers {
 
     @Container
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(POSTGRES_IMAGE)
+            .withNetworkMode("jenkins-exchange-rate-service-testcontainers-8_default")
             .withDatabaseName(DATABASE_NAME)
             .withUsername(DATABASE_USER)
             .withPassword(DATABASE_PASSWORD)
@@ -57,6 +58,7 @@ public class ExchangeRateServiceTestContainers {
     @Container
     static GenericContainer<?> wireMockContainer = new GenericContainer<>(
             DockerImageName.parse("wiremock/wiremock:3.13.1"))
+            .withNetworkMode("jenkins-exchange-rate-service-testcontainers-8_default")
             .withFileSystemBind("src/main/resources/wiremock/mappings",
                     "/home/wiremock/mappings", BindMode.READ_ONLY)
             .withExposedPorts(8080)
