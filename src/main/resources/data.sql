3edc6f1b2d80a519e73a2608d579600a1e1a491b
dany.cenas <dany.cenas@bbva.com>
2025-07-24 20:28:28 -0500

    .

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO exchange_rates (currency_pair, rate) VALUES
-    ('USD_PEN', 3.60),
+    ('USD_PEN', 3.70),
     ('EUR_PEN', 4.10),
     ('CNY_PEN', 0.50)
 ON CONFLICT (currency_pair) DO NOTHING;
